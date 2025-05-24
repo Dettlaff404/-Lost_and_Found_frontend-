06749a15fe9e86e268ceedae4d78fa9d1d195619
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-05-25 04:24:31 +0530

    Created Requests table template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavB from './components/NavBar/NavB';
+import { RequestConsole } from './components/RequestConsole';
 
 function App() {
   return (
     <>
       <NavB/>
+      <RequestConsole/>
     </>
   );
 }
--- src/components/RequestConsole.tsx
+++ src/components/RequestConsole.tsx
@@ -1,7 +1,67 @@
+import Table from 'react-bootstrap/Table';
+
 export function RequestConsole() {
+
+    const tHeads: string[] = [
+        "Request ID",
+        "User ID",
+        "Item Name", 
+        "Description", 
+        "Location", 
+        "Date", 
+        "Item Status", 
+        "Request Status", 
+    ];
+
     return (
         <>
-            <p>RequestConsole</p>
+            <Table responsive="md" striped bordered hover>
+                <thead>
+                    <tr>
+                        {tHeads.map((headings) => (
+                            <th>{headings}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                </tbody>
+            </Table>
         </>
     );
 }
